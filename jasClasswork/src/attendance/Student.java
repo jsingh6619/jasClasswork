12afe5e1850aa94d207642a43fd2573d17188683
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-09-27 14:40:49 -0400

    09/27/2017

--- jasClasswork/src/attendance/Student.java
+++ jasClasswork/src/attendance/Student.java
@@ -16,10 +16,11 @@ public class Student implements Attendee {
 		else {
 			return false;
 		}
+		// return isPresent; could also work
 	}
 	
 	public void setPresent(boolean present) {
-		isPresent = true;
+		isPresent = present;
 	}
 	
 	public String getFirstName() {
@@ -31,24 +32,51 @@ public class Student implements Attendee {
 	}
 	
 	public boolean mathces(String first, String last) {
-		if(this.first == first && this.last == last) {
+		return first.toLowerCase().equals(this.first.toLowerCase()) && last.toLowerCase().equals(this.last.toLowerCase());
+		/*if(this.firstName == first && this.lastName == last) {
 			return true;
 		}
 		else {
 			return false;
 		}
+		
+		never use == for Strings
+		*/
 	}
 	
 	public boolean matches(String last) {
-		if(this.last == last) {
+		return last.toLowerCase().equals(this.last.toLowerCase());
+		/*if(this.lastName == last) {
 			return true;
 		}
 		else {
 			return false;
 		}
+		*/
 	}
 	
 	public String getReportString() {
-		
+		String report = last;
+		while (report.length()<20) {
+			report += " ";
+		}
+		report += first;
+		while (report.length()<40) {
+			report += " ";
+		}
+		if(isPresent) {
+			report+="PRESENT/n";
+		}
+		else {
+			report+="ABSENT/n";
+		}
+		return report;
+		/*String end = this.lastName;
+		while(abc.length < 20) {
+			String end += " ";
+		}
+		String end += this.firstName;
+		return end;
+		*/
 	}
 }
