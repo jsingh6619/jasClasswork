18e4e984dc5ca389d1a121a53b2ef8ffb48c2472
Jas <Jas@jasmits-mbp.home>
2017-09-26 23:11:33 -0400

    9/28

--- jasClasswork/src/attendance/Student.java
+++ jasClasswork/src/attendance/Student.java
@@ -1,27 +1,38 @@
 package attendance;
 
 public class Student implements Attendee {
+	private boolean isPresent = false;
 	private String first;
 	private String last;
-			
+	
 	public boolean isPresent() {
-		
+		if (isPresent = true) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	public void setPresent(boolean present) {
-		
+		isPresent = true;
 	}
 	
 	public String getFirstName() {
-		
+		return first;
 	}
 	
 	public String getLastName() {
-		
+		return last;
 	}
 	
 	public boolean mathces(String first, String last) {
-		
+		if(this.first == first && this.last == last) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	public boolean matches(String last) {
