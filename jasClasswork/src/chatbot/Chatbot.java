6c8e8ae496d75a8b60c7ce8342a1c1a4c7a9e04b
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-09-28 15:34:33 -0400

    09/28/2017 Begin Project

--- jasClasswork/src/chatbot/Chatbot.java
+++ jasClasswork/src/chatbot/Chatbot.java
@@ -1,9 +1,34 @@
 package chatbot;
 
 public class Chatbot {
+	
+	private String username;
+	private boolean chatting;
+	private Topic jasmit;
 
 	public Chatbot() {
-		// TODO Auto-generated constructor stub
+		jasmit = new ChatbotJasmit();
+		username = "Unknown User";
+		chatting = true;
+	}
+
+	public void startChatting() {
+		//whenever you print or get input, use these methods
+		ChatbotMain.print("Hi! I am an intelligent machine that can respond to your input. Tell me your name.");
+		username = ChatbotMain.getInput();
+		
+		while(chatting) {
+			ChatbotMain.print("What would you like to talk about");
+			String response = ChatbotMain.getInput();
+			
+			if(jasmit.isTriggered(response)) {
+				chatting = false;//exits the while loop. IMPORTANT you get graded for this!
+				jasmit.talk(response);
+			}
+			else {
+				ChatbotMain.print("I'm sorry. I dont understand. I never said I was perfect.");
+			}
+		}
 	}
 
 }
