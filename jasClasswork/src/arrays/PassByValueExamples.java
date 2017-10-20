63a9ee9ecb617c5764bb810b9a13096d07804107
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-10-20 14:59:55 -0400

    10/20/2017

---
+++ jasClasswork/src/arrays/PassByValueExamples.java
@@ -0,0 +1,57 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class PassByValueExamples {
+
+	public static void main(String[] args) {
+		String s = "Hello";
+		Person p = new Person("Random", "Dude", Borough.NY_BOROUGHS[0]);
+		int x = 5;
+		int[] arr = {1,2,3};
+		
+		test3(arr);
+		System.out.println("p is now " + p + ", x is " + x + ", arr is " + Arrays.toString(arr));
+	}
+
+	/**
+	 * You can change an object's reference via its SETTERS (methods that set fields)
+	 * @param p
+	 */
+	private static void test2(Person p) {
+		p.setFirstName("Original");
+	}
+		
+	/**
+	 * This is how you can change arrays via the local variable:
+	 * through its references (i.e. indices)
+	 * because primitives don't reference other data (hence their name, "primitives"), it is not possible to change them via a local variable like we did with Objects and arrays
+	 * @param arr
+	 */
+	private static void test3 (int[] arr) {
+		arr[0] = 999;
+		arr[1] = 998;
+	}
+	
+	//failed methods
+	
+	/**
+	 * It is not possible to change an original reference via a local variable as in the example below
+	 * @param p
+	 * @param x
+	 * @param arr
+	 */
+	private static void test1(Person p, int x, int[] arr) {
+		String name = p.getFirstName();
+		name = "Original";
+	}
+
+	private static void changeEVERYTHING(String s, int x, int[] arr) {
+		s = "Goodbye";
+		x = -5;
+		arr = new int[2];
+		arr[0] = -1;
+		arr[1] = -2;
+	}
+
+}
--- jasClasswork/src/arrays/Person.java
+++ jasClasswork/src/arrays/Person.java
@@ -3,9 +3,9 @@ package arrays;
 public class Person {
 	//first part would be like jas
 	//second would be like mit
-	public static final String[] FIRST_START = {"Jas", "Har", "Gur", "Aman", "Harsh"};
-	public static final String[] FIRST_MIDDLE = {"ala", "ima", "e", "o", "anna", "mi", "ola"};
-	public static final String[] FIRST_END = {"meet", "preet", "deep", "winder", "rian", "lius", "lion"};
+	public static final String[] FIRST_START = {"Ge", "Jos", "Gur", "Stor" };
+	public static final String[] FIRST_MIDDLE = {"a", "e", "i", "o", "u"};
+	public static final String[] FIRST_END = {"let", "rge", "rian", "lius", "lion"};
 	// Could just be singh and kaur
 	public static final String[] LAST_START = {"Bl", "Gr", "Ph", "M", "Thr", "Sh", "Sin"};
 	public static final String[] LAST_MIDDLE = {"an", "g", "ast", "own", "il"};
@@ -16,6 +16,7 @@ public class Person {
 	private Borough home;
 	private Hobby hobby;
 	private Person[] friends;
+	private String nickname;
 	
 	public Person(String first, String last, Borough home) {
 		this.firstName = first;
@@ -23,6 +24,53 @@ public class Person {
 		this.home = home;
 		friends = new Person[3];
 		hobby = Hobby.randomHobby();
+		nickname = createNickname(firstName);
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	/**
+	 * PASS BY VALUE
+	 * the parameters of a method contain only values, not references, therefore when they are changed, the REFERENCE to the ORIGINAL object does not change
+	 *  
+	 * returns a String equal to name up to the index of (but not including) the second vowel
+	 * createNickname("Jonathan") -> "Jon"
+	 * @param name
+	 * @return
+	 */
+	public static String createNickname(String name) {
+		String nickname = "";
+		int count = 0;
+		for(int i = 0; i < name.length(); i++) {
+			String letter = name.substring(i, i+1);
+			if(isVowel(letter)) {
+				count++;
+				if(count != 2) {
+					nickname += letter;
+				}
+				else {
+					return nickname;
+				}
+			}
+			else {
+				// add the letter when not a vowel
+				nickname += letter;
+			}
+		}
+		return nickname;
+	}
+		
+	public static boolean isVowel(String letter) {
+		letter = letter.toLowerCase();
+		return letter.equals("a")||letter.equals("e")||letter.equals("i")||letter.equals("o")||letter.equals("u");
 	}
 	
 	public void stateYourFriends() {
@@ -53,6 +101,6 @@ public class Person {
 	}
 	
 	public String toString() {
-		return "My name is " + firstName + " " + lastName + " and I am from " + home + ".";
+		return "My name is " + firstName + " " + lastName + ". Call me " + nickname + " and I am from " + home + ".";
 	}
 }
