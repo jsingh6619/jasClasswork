aaf4244e7bb2993444ed8bdedd0e1f6ed0803af0
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-10-27 14:58:14 -0400

    10/27/2017

---
+++ jasClasswork/src/caveExplorer/Inventory.java
@@ -0,0 +1,14 @@
+package caveExplorer;
+
+public class Inventory {
+
+	public void updateMap() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public String getDescription() {
+		return "There is nothing in your inventory.";
+	}
+
+}
