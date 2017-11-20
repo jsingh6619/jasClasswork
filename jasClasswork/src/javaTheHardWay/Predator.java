221391ea20112092e514cb6ea13bca7dae057f19
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-11-20 14:27:20 -0500

    11/20

---
+++ jasClasswork/src/javaTheHardWay/Predator.java
@@ -0,0 +1,10 @@
+package javaTheHardWay;
+
+public class Predator extends Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ jasClasswork/src/javaTheHardWay/Prey.java
@@ -0,0 +1,10 @@
+package javaTheHardWay;
+
+public class Prey extends Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
